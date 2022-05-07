9e7a796bc1c336e5dcd92d5307ff252bbb92223e
Zakaria Yaris <info@zakariya.me>
2022-05-07 23:38:18 +0100

    7 May 11:38 pm

--- codewarsJavascript.js
+++ codewarsJavascript.js
@@ -814,3 +814,12 @@ function areYouPlayingBanjo(name) {
   return name.startsWith('R') || name.startsWith('r') ?
     `${name} plays banjo` : `${name} does not play banjo`
 }
+
+
+// 80
+// Sum Arrays
+function sum(numbers) {
+  "use strict";
+
+  return numbers.length > 0 ? numbers.reduce((a, b) => a + b, 0) : 0
+};
