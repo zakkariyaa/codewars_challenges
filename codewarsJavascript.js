a7f9eb19cebe0f04ae6865b93872cbe54e492c7c
Zakaria Yaris <info@zakariya.me>
2022-05-27 23:15:15 +0100

    27 May 10:08 pm

--- codewarsJavascript.js
+++ codewarsJavascript.js
@@ -1040,3 +1040,14 @@ function points(games) {
 function stringToArray(string) {
   return string.split(' ')
 }
+
+
+// 105
+// Count by X
+function countBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i++) {
+    z.push(x * i)
+  }
+  return z;
+}
