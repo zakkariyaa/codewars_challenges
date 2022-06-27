084f0ce919a4bfc892d6fdc5a69d9a724c54ec38
Zakaria Yaris <info@zakariya.me>
2022-06-27 09:34:11 +0100

    update

--- codewars_python.py
+++ codewars_python.py
@@ -780,3 +780,8 @@ def lovefunc( flower1, flower2 ):
 # Beginner Series #1 School Paperwork
 def paperwork(n, m):
     return 0 if n < 0 or m < 0 else n * m
+
+# 102
+# Fake Binary
+def fake_bin(x):
+    return ''.join('0' if int(char) < 5 else '1' for char in x)
