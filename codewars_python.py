8abd9671cbdeaa9400f32dd5affbbc8657fa9051
Zakaria Yaris <info@zakariya.me>
2022-09-25 09:34:11 +0100

    update

--- codewars_python.py
+++ codewars_python.py
@@ -1211,3 +1211,16 @@ def pipe_fix(nums):
 # Capitalization and Mutability
 def capitalize_word(word):
     return word.capitalize()
+
+# 167
+# 101 Dalmatians - squash the bugs, not the dogs!
+def how_many_dalmatians(n):
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    if n <= 10:
+        return dogs[0]
+    elif n <= 50:
+        return dogs[1]
+    elif n == 101:
+        return dogs[3]
+    else:
+        return dogs[2]
